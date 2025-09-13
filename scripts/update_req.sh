c99782bef509dd409cc5e31f2bebf60befedce20
Elimelech70 <craigjcolley@gmail.com>
2025-09-13 02:10:05 +0000

    update requirements.txt

--- scripts/update_req.sh
+++ scripts/update_req.sh
@@ -1,7 +1,7 @@
-cp /tmp/fixed_requirements.txt services/orchestration/requirements.txt
-cp /tmp/fixed_requirements.txt services/scanner/requirements.txt
-cp /tmp/fixed_requirements.txt services/pattern/requirements.txt
-cp /tmp/fixed_requirements.txt services/technical/requirements.txt
-cp /tmp/fixed_requirements.txt services/trading/requirements.txt
-cp /tmp/fixed_requirements.txt services/reporting/requirements.txt
-cp /tmp/fixed_requirements.txt services/news/requirements.txt
+cp ./tmp/fixed_requirements.txt ./services/orchestration/requirements.txt
+cp ./tmp/fixed_requirements.txt ./services/scanner/requirements.txt
+cp ./tmp/fixed_requirements.txt ./services/pattern/requirements.txt
+cp ./tmp/fixed_requirements.txt ./services/technical/requirements.txt
+cp ./tmp/fixed_requirements.txt ./services/trading/requirements.txt
+cp ./tmp/fixed_requirements.txt ./services/reporting/requirements.txt
+cp ./tmp/fixed_requirements.txt ./services/news/requirements.txt
